61ae7982726706d49ac5789dc6fd63275737730c
pororonron <153398697+pororonron@users.noreply.github.com>
2024-01-30 16:33:34 +0000

    [Add]IndexページのEditリンク

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -7,4 +9,30 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  def create
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
