bae032a1c04d6e6d9a694b6451febc3635b638ab
pororonron <153398697+pororonron@users.noreply.github.com>
2024-01-31 02:44:22 +0000

    [Add]Showページのレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,11 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
